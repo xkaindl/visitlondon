8a157552ba0947b90b9b7ef17568cf644fc599e1
Xaver Kaindl <x.kaindl@TEKAEF.local>
2017-07-04 15:12:16 +0200

    coy task in the gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,12 @@ var gulp = require("gulp");
 var sass = require("gulp-sass");
 var browserSync = require('browser-sync');
 var reload = browserSync.reload;
+var autoprefixer = require('gulp-autoprefixer');
 
-var SOUREPATHS = {
+var SOURCEPATHS = {
 
-	sassSource : 'src/scss/*.scss'
+	sassSource : 'src/scss/*.scss',
+	htmlSource: 'src/*.html'
 }
 var APPPATH = {
 	root: 'app/',
@@ -15,10 +17,16 @@ var APPPATH = {
 
 
 gulp.task('sass', function(){
-	return gulp.src(SOUREPATHS.sassSource)
-		.pipe(sass({outputStyle: 'expanded'}).on('errer', sass.logError))
+	return gulp.src(SOURCEPATHS.sassSource)
+		.pipe(autoprefixer())
+		.pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
 		.pipe(gulp.dest(APPPATH.css));
 });
+
+gulp.task('copy', function(){
+	gulp.src(SOURCEPATHS.htmlSource)
+	.pipe(gulp.dest(APPPATH.root))
+})
 
 gulp.task('serve', ['sass'], function(){
 	browserSync.init([APPPATH.css + '/*.css', APPPATH.root + '/*.html', APPPATH.js + '/*.js'], {
@@ -28,8 +36,9 @@ gulp.task('serve', ['sass'], function(){
 	})
 });
 
-gulp.task('watch', ['serve', 'sass'], function(){
-	gulp.watch([SOUREPATHS.sassSource], ['sass']);
+gulp.task('watch', ['serve', 'sass', 'copy'], function(){
+	gulp.watch([SOURCEPATHS.sassSource], ['sass']);
+	gulp.watch([SOURCEPATHS.htmlSource], ['copy']);
 })
 
 gulp.task('default', ['watch']);
