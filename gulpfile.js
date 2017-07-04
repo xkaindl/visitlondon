b7cf7dc0f8aeec2d9192249d4eb35bad504ab5e4
Xaver Kaindl <x.kaindl@TEKAEF.local>
2017-07-04 13:50:41 +0200

    gulps-sass and gulp added

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,10 @@
-gulpfile.js
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+
+gulp.task('sass', function(){
+	return gulp.src('src/scss/app.scss')
+		.pipe(sass({outputStyle: 'expanded'}).on('errer', sass.logError))
+		.pipe(gulp.dest('app/css'));
+});
+
+gulp.task('default', ['sass']);
